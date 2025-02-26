e73220c03f141082563d97a31795a9239442daef
IsmaelKP2 <ipapa@splunk.com>
2025-02-26 18:49:01 +0000

    remove authentication placed in old for now

--- frontend/streamlit_app.old.py
+++ frontend/streamlit_app.old.py
@@ -86,42 +86,65 @@ def upload_file(file, bucket, object_name=None):
         return False
     return True
 
+# Create OAuth2Component instance
+oauth2 = OAuth2Component(CLIENT_ID, CLIENT_SECRET, AUTHORIZE_URL, TOKEN_URL, REFRESH_TOKEN_URL, REVOKE_TOKEN_URL)
 
-st.markdown("### Upload a picture of a cat and I will tell you how many cats there are in the picture")
+# Check if token exists in session state
+if 'token' not in st.session_state:
+    # If not, show authorize button
+    result = oauth2.authorize_button("Authorize", REDIRECT_URI, SCOPE)
+    if result and 'token' in result:
+        # If authorization successful, save token in session state
+        st.session_state.token = result.get('token')
+        st.rerun()
+else:
+    # If token exists in session state, show the token
+    token = st.session_state['token']
+    #st.json(token)
 
-st.markdown("#### available pictures:")
-
-load_images()
-## Load images from s3 in a grid
-images = load_images()
-
-uploaded_file = st.file_uploader("Choose a picture :",type=["jpg", "png"])
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-
-    with st.spinner('Uploading to s3...'):
-        result = upload_file(uploaded_file,f"{AWS_BUCKET}",uploaded_file.name)
-        if result:
-            st.write('File was uploaded successfully')
+    st.markdown("### Upload a picture of a cat and I will tell you how many cats there are in the picture")
+    
+    st.markdown("#### available pictures:")
+    if st.button("refresh"):
+        load_images()
+    ## Load images from s3 in a grid
+    images = load_images()
+    
+    uploaded_file = st.file_uploader("Choose a picture :",type=["jpg", "png"])
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+    
+        with st.spinner('Uploading to s3...'):
+            result = upload_file(uploaded_file,f"{AWS_BUCKET}",uploaded_file.name)
+            if result:
+                st.write('File was uploaded successfully')
+            else:
+                st.write('File was not uploaded successfully')
+    
+        # Convert bytes_data to BytesIO object
+        bytes_io = BytesIO(bytes_data)
+        st.image(bytes_io,width=300)
+    
+        # Prepare the files dictionary for the request
+        files = {
+            "file": (uploaded_file.name, bytes_data, uploaded_file.type)
+        }
+    
+        response = requests.post(url, params=params, headers=headers, files=files)
+        message = st.chat_message("assistant")
+        if response.status_code == 200:
+            message.write(f"The predicted answer is {response.json().get('answer')}")
         else:
-            st.write('File was not uploaded successfully')
+            st.write("Error:", response.json())
 
-    # Convert bytes_data to BytesIO object
-    bytes_io = BytesIO(bytes_data)
-    st.image(bytes_io,width=300)
+    if st.button("Refresh Token"):
+        # If refresh token button is clicked, refresh the token
+        token = oauth2.refresh_token(token)
+        st.session_state.token = token
+        st.rerun()
 
-    # Prepare the files dictionary for the request
-    files = {
-        "file": (uploaded_file.name, bytes_data, uploaded_file.type)
-    }
 
-    response = requests.post(url, params=params, headers=headers, files=files)
-    message = st.chat_message("assistant")
-    if response.status_code == 200:
-        message.write(f"The predicted answer is {response.json().get('answer')}")
-    else:
-        st.write("Error:", response.json())
 
 
 
